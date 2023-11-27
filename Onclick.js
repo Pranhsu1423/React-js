ebd780fe867bd5d1f30d078883fdf562b0efc6ba
PRANSHU SINGH <149379985+Pranshusingh24@users.noreply.github.com>
2023-11-27 12:52:46 +0530

    React Events (onClick, onChange, etc)

---
+++ Onclick.js
@@ -0,0 +1,69 @@
+// import React from 'react'
+
+// //  <> functional Component </>
+
+// function Onclick(props) {
+
+//     // function chickhere(){
+//     //     alert('Hello I am Pranshu');
+//     // }
+
+//     // <> with arrow function  </>
+//     var clickhere = (name) => {
+//         alert ('this is Arrow Function Writter ' +  name);
+//     }
+
+//   return (
+//     <div>
+//         <button type='text' value= 'Click Here' onClick={
+//             () => clickhere(props.name)
+//         }>Click Here </button>
+//     </div>
+//   )
+// }
+// **************************//
+// export default Onclick
+
+
+// import React, { Component } from "react";
+
+// // <> Class Component </>
+// export default class Onclick extends Component {
+
+//   chickhere() {
+//     alert("Hello I am Pranshu");  
+// }
+  
+//   render() {
+//     var clickhere = (name) => {
+//         alert ('this is Arrow Function Writter ' +  name);
+//     }
+//     return (
+//       <div>
+//         <button type="text" value="Click Here" onClick={ () => clickhere("Pranshu Singh")}>
+//           Click Here 
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// **************************//
+// import React, { Component } from 'react'
+
+// //   <> functional Component </>
+
+// function Onclick() {
+
+//     function chickhere(){
+//         alert('Hello I am Pranshu');
+//     }
+
+//   return (
+//     <div>
+//         <button type='text' value= 'Click Here' onClick={chickhere}>Click Here </button>
+//     </div>
+//   )
+// }
+
+// export default Onclick
